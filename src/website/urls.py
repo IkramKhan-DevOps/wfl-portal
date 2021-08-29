b19400935183f2b8c35a365304b9d47ac69a02d0
IkramKhan-DevOps <ikram.khan0762@gmail.com>
2021-08-29 18:17:43 +0500

    WEBSITE: Ico, Favicons and Exarth Icons added

--- src/website/urls.py
+++ src/website/urls.py
@@ -4,4 +4,5 @@ from django.views.generic import TemplateView
 app_name = 'website'
 urlpatterns = [
     path('', TemplateView.as_view(template_name='website/home.html')),
+    path('404/', TemplateView.as_view(template_name='website/404.html')),
 ]
